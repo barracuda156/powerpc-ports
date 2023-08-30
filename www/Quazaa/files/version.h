a6bf2626cda8ef68aaf7253d6a98d05e6b4e3a02
barracuda156 <vital.had@gmail.com>
2023-08-30 18:46:08 +0800

    Quazaa: wip

---
+++ www/Quazaa/files/version.h
@@ -0,0 +1,12 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+namespace Version
+{
+	static const int MAJOR = 0;
+	static const int MINOR = 1;
+	static const char* REVISION = "";
+	static const char* BUILD_DATE = "2018-11-01";
+}
+
+#endif // VERSION_H
