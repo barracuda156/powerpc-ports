8367f68465d7e86ff6e8802b713492a9ee1a8ad9
Sergey Fedorov <barracuda@macos-powerpc.org>
2024-07-05 14:16:40 +0800

    gcc_compilers: support gcc-powerpc

--- _resources/port1.0/compilers/gcc_compilers.tcl
+++ _resources/port1.0/compilers/gcc_compilers.tcl
@@ -3,10 +3,7 @@
 # https://trac.macports.org/ticket/57135
 # https://trac.macports.org/ticket/61636
 
-# GCC 10 and above on OSX10.6+
-if {${os.major} >= 10 || [option os.platform] ne "darwin"} {
-    lappend compilers macports-gcc-14 macports-gcc-13 macports-gcc-12 macports-gcc-11 macports-gcc-10
-}
+lappend compilers macports-gcc-14 macports-gcc-13 macports-gcc-12 macports-gcc-11 macports-gcc-10
 
 # GCC 9 and older only on OSX10.10 and older
 # https://trac.macports.org/ticket/65472
@@ -19,4 +16,9 @@ if {${os.major} < 15} {
 
 if {${os.major} >= 10} {
     lappend compilers macports-gcc-devel
+}
+
+global os.arch
+if {${os.arch} eq "powerpc"} {
+    lappend compilers macports-gcc-powerpc
 }
